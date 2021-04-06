84fd95e1c7b8e5e6e510ee4391c2537985a00242
rameez471 <yashpathak29@gmail.com>
2021-04-06 16:30:46 +0530

    O-Net

--- model/network.py
+++ model/network.py
@@ -35,7 +35,7 @@ def RNet():
 
     x_input = Input((24,24,3))
 
-    x = Permute([2,1,3],name='rnet_permute_1')(x_input)
+    x = Permute((2,1,3),name='rnet_permute_1')(x_input)
     x = Conv2D(28,3,1,name='rnet_conv_1')(x)
     x = PReLU(shared_axes=[1,2,3],name='rnet_prelu_1')(x)
     x = MaxPool2D(3,2,padding='same',name='rnet_maxpool_1')(x)
@@ -63,7 +63,45 @@ def RNet():
     return model
 
 
-model = RNet()
+def ONet():
 
-with open('../data/r_net_summary.txt','w') as f:
+    x_input = Input((48,48,3))
+
+    x = Permute([2,1,3],name='onet_permute')(x_input)
+    x = Conv2D(32,3,1,name='onet_conv_1')(x)
+    x = PReLU(shared_axes=[1,2],name='onet_prelu_1')(x)
+    x = MaxPool2D(3,2,padding='same',name='onet_maxpool_1')(x)
+
+    x = Conv2D(64,3,1,name='onet_conv_2')(x)
+    x = PReLU(shared_axes=[1,2],name='rnet_prelu_2')(x)
+    x = MaxPool2D(3,2,name='onet_maxpool_2')(x)
+
+    x = Conv2D(64,3,1,name='onet_conv_3')(x)
+    x = PReLU(shared_axes=[1,2],name='onet_prelu_3')(x)
+    x = MaxPool2D(2,2,padding='same',name='onet_maxpool_3')(x)
+
+    x = Conv2D(128,2,1,name='onet_conv_4')(x)
+    x = PReLU(shared_axes=[1,2],name='onet_prelu_4')(x)
+
+    x = Flatten(name='onet_flatten')(x)
+    x = Dense(256,name='oenet_dense')(x)
+    x = PReLU(name='onet_prelu_5')(x)
+
+    # Output 1 
+    a = Dense(2, name='output_1')(x)
+
+    #Output 2
+    b = Dense(4, name='output_2')(x)
+
+    #Output 3
+    c = Dense(10, name='output_3')(x)
+
+    model = Model(inputs=x_input, outputs=[a,b,c])
+
+    return model
+
+
+model = ONet()
+
+with open('../data/o_net_summary.txt','w') as f:
     model.summary(print_fn=lambda x: f.write(x+'\n'))
